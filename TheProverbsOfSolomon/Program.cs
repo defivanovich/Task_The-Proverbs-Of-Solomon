4845e1b55fc38c3610a9f730dc2a82db6c54bad5
def <fedyaxne@narod.ru>
2016-01-08 14:18:23 +0200

    подправил названия папок

--- TheProverbsOfSolomon/Program.cs
+++ TheProverbsOfSolomon/Program.cs
@@ -12,10 +12,10 @@ namespace TheProverbsOfSolomon
         static void Main(string[] args)
         {
 
-            FileInfo f = new FileInfo("G:\\Git\\ConsoleApplication2\\Files\\output\\base_output.txt");
+            FileInfo f = new FileInfo("G:\\Git\\Files\\output\\base_output.txt");
             StreamWriter w = f.CreateText();
 
-            using (var sr = new StreamReader("G:\\Git\\ConsoleApplication2\\Files\\input\\base.txt", Encoding.GetEncoding(1251)))
+            using (var sr = new StreamReader("G:\\Git\\Files\\input\\base.txt", Encoding.GetEncoding(1251)))
             {
                 string read = null;
                 while ((read = sr.ReadLine()) != null)
