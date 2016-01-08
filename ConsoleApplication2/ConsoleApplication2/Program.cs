f3ad0e9bda8736677a20d5611cf4cea5be4c0ede
def <fedyaxne@narod.ru>
2016-01-08 13:10:18 +0200

    первая "кривая"сборка

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            FileInfo f = new FileInfo("G:\\Git\\ConsoleApplication2\\Files\\base_output.txt");
+            StreamWriter w = f.CreateText();
+
+            using (var sr = new StreamReader("G:\\Git\\ConsoleApplication2\\Files\\base.txt", Encoding.GetEncoding(1251)))
+            {
+                string read = null;
+                while ((read = sr.ReadLine()) != null)
+                { 
+                    char ch = ')';
+                    int indexOfChar = read.IndexOf(ch);
+
+                    Console.WriteLine(read);
+                    read = read.Substring(indexOfChar + 1);
+                    read = read.Trim();
+                    Console.WriteLine(read);
+                string newS = "<item>" + read + "</item>";
+                Console.WriteLine(newS);
+                w.WriteLine(newS);
+                w.Write(w.NewLine);
+
+                }
+
+            }
+            w.Close();
+            Console.ReadKey();
+        }
+    }
+}
