9655e1c50d7bc789bc001e6effe0c353c58d5050
def <fedyaxne@narod.ru>
2016-01-08 16:12:05 +0200

    много информации

--- TheProverbsOfSolomon/Program.cs
+++ TheProverbsOfSolomon/Program.cs
@@ -11,31 +11,31 @@ namespace TheProverbsOfSolomon
     {
         static void Main(string[] args)
         {
+            string read = null;
+            char ch = ')';
 
             FileInfo f = new FileInfo("G:\\Git\\Projects\\Files\\base_output.txt");
             StreamWriter w = f.CreateText();
 
             using (var sr = new StreamReader("G:\\Git\\Projects\\Files\\input\\base.txt", Encoding.GetEncoding(1251)))
             {
-                string read = null;
+                
                 while ((read = sr.ReadLine()) != null)
-                { 
-                    char ch = ')';
+                {
+                   
                     int indexOfChar = read.IndexOf(ch);
 
-                    Console.WriteLine(read);
-                    read = read.Substring(indexOfChar + 1);
-                    read = read.Trim();
-                    Console.WriteLine(read);
-                string newS = "<item>" + read + "</item>";
-                Console.WriteLine(newS);
-                w.WriteLine(newS);
-                w.Write(w.NewLine);
+                    read = read.Substring(indexOfChar + 1).Trim();
+           
+                    var newS = String.Format("<item>{0}</item>", read);       
+                    w.WriteLine(newS);
+                    w.Write(w.NewLine);
 
                 }
 
             }
             w.Close();
+            Console.WriteLine("Готово!");
             Console.ReadKey();
         }
     }
