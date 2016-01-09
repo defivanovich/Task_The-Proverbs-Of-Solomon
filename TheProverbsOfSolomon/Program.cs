0bbaabca69589e008aa43d918a8b39f1bc3409e1
def <fedyaxne@narod.ru>
2016-01-09 11:05:03 +0200

    новые классы

--- TheProverbsOfSolomon/Program.cs
+++ TheProverbsOfSolomon/Program.cs
@@ -7,34 +7,53 @@ using System.IO;
 
 namespace TheProverbsOfSolomon
 {
-    class Program
+
+    public class Converter
     {
-        static void Main(string[] args)
+        public string inputFileName, outputFileName;
+        public Converter(string inputFileName, string outputFileName)
+        {
+            this.inputFileName = inputFileName;
+            this.outputFileName = outputFileName;
+        }
+
+        public void Convert()
         {
             string read = null;
             char ch = ')';
 
-            FileInfo f = new FileInfo("G:\\Git\\Projects\\Files\\base_output.txt");
+            FileInfo f = new FileInfo(outputFileName);
             StreamWriter w = f.CreateText();
 
-            using (var sr = new StreamReader("G:\\Git\\Projects\\Files\\input\\base.txt", Encoding.GetEncoding(1251)))
+            using (var sr = new StreamReader(inputFileName, Encoding.GetEncoding(1251)))
             {
-                
+
                 while ((read = sr.ReadLine()) != null)
                 {
-                   
-                    int indexOfChar = read.IndexOf(ch);
 
+                    int indexOfChar = read.IndexOf(ch);
                     read = read.Substring(indexOfChar + 1).Trim();
-           
-                    var newS = String.Format("<item>{0}</item>", read);       
+
+                    var newS = String.Format("<item>{0}</item>", read);
                     w.WriteLine(newS);
                     w.Write(w.NewLine);
 
                 }
-
             }
             w.Close();
+
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputFileName = "G:\\Git\\Projects\\Files\\input\\base.txt";
+            var outputFileName = "G:\\Git\\Projects\\Files\\output\\base_output.txt";
+            var converter = new Converter(inputFileName, outputFileName);
+            converter.Convert();
             Console.WriteLine("Готово!");
             Console.ReadKey();
         }
